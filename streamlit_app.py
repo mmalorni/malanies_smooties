9faed9a71fe2d4b8da7c91539454770f6b13686a
Martin Malorni <mmalorni@gmail.com>
2024-10-08 12:27:02 -0400

    Update streamlit_app.py
    forgot to comment out st.stop()
    Also commented out the api call since the server is down.

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('fruit
 
 # Convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+# st.dataframe(pd_df)
+# st.stop()
 
 ingredient_list = st.multiselect(
     "Choose up to 5 ingredients:",
@@ -37,8 +37,8 @@ if ingredient_list:
         st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
 
         st.subheader(fruit_chosen + ' Nutrition Information')
-        #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
-        #fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+        # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + search_on)
+        # fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
     # st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
